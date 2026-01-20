39919f2981e7d95342de4a2a67d5f5837ce71c41
Gökçe Gökben <138039805+GokceGokben@users.noreply.github.com>
2026-01-20 13:01:02 +0300

    removed unnecessary command lines

--- utility.py
+++ utility.py
@@ -8,10 +8,9 @@ def extract_frames(gif_path, output_folder):
     with Image.open(gif_path) as im:
         for i in range(im.n_frames):
             im.seek(i)
-            # Kareyi şeffaflığı koruyarak kaydet
             frame = im.convert("RGBA")
             frame.save(f"{output_folder}/frame_{i:03d}.png")
-    print(f"{im.n_frames} kare başarıyla çıkarıldı!")
+    print(f"{im.n_frames} frame is extracted successfully!")
 
-# Kullanımı:
+# Usage:
 extract_frames("flower.gif", "frames")
